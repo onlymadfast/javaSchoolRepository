3529f50e5cc64ff9cec0b9ebb8fed76159e4d1b9
onlymadfast <tsipadan.dan@gmail.com>
2021-02-25 16:07:45 +0300

    commit

--- javaschool/src/main/java/com/tsipadan/controller/GlobalExceptionHandler.java
+++ javaschool/src/main/java/com/tsipadan/controller/GlobalExceptionHandler.java
@@ -1,54 +1,54 @@
-//package com.tsipadan.controller;
-//
-//import com.tsipadan.exception.ResourceCreationException;
-//import com.tsipadan.exception.ResourceUpdateException;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//@Slf4j
-//@ControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//  /**
-//   * Default handler for all exceptions
-//   *
-//   * @param request - request
-//   * @param e - exception
-//   * @return error.jsp
-//   */
-//  @ExceptionHandler(value = Exception.class)
-//  public String getAllException(HttpServletRequest request, Exception e){
-//    log.warn("Request: " + request.getRequestURI() + " catch something like -> " + e);
-//    return "error";
-//  }
-//
-//  /**
-//   * Handler for Create exception
-//   *
-//   * @param request - request
-//   * @param e - exception
-//   * @return error.jsp
-//   */
-//  @ExceptionHandler(value = ResourceCreationException.class)
-//  public String getResourceCreationException(HttpServletRequest request, Exception e){
-//    log.warn("Request: " + request.getRequestURI() + " catch something like -> " + e);
-//    return "error";
-//  }
-//
-//  /**
-//   * Handler for Update exception
-//   *
-//   * @param request - request
-//   * @param e - exception
-//   * @return error.jsp
-//   */
-//  @ExceptionHandler(value = ResourceUpdateException.class)
-//  public String getResourceUpdateException(HttpServletRequest request, Exception e){
-//    log.warn("Request: " + request.getRequestURI() + " catch something like -> " + e);
-//    return "error";
-//  }
-//
-//}
+package com.tsipadan.controller;
+
+import com.tsipadan.exception.ResourceCreationException;
+import com.tsipadan.exception.ResourceUpdateException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  /**
+   * Default handler for all exceptions
+   *
+   * @param request - request
+   * @param e - exception
+   * @return error.jsp
+   */
+  @ExceptionHandler(value = Exception.class)
+  public String getAllException(HttpServletRequest request, Exception e){
+    log.warn("Request: " + request.getRequestURI() + " catch something like -> " + e);
+    return "error";
+  }
+
+  /**
+   * Handler for Create exception
+   *
+   * @param request - request
+   * @param e - exception
+   * @return error.jsp
+   */
+  @ExceptionHandler(value = ResourceCreationException.class)
+  public String getResourceCreationException(HttpServletRequest request, Exception e){
+    log.warn("Request: " + request.getRequestURI() + " catch something like -> " + e);
+    return "error";
+  }
+
+  /**
+   * Handler for Update exception
+   *
+   * @param request - request
+   * @param e - exception
+   * @return error.jsp
+   */
+  @ExceptionHandler(value = ResourceUpdateException.class)
+  public String getResourceUpdateException(HttpServletRequest request, Exception e){
+    log.warn("Request: " + request.getRequestURI() + " catch something like -> " + e);
+    return "error";
+  }
+
+}
